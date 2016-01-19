7e9e082ea31a266e910cb2bab01c11c48abc481b
mdakram28 <mdakram28@gmail.com>
2016-01-19 22:25:30 +0000

    sdfs

--- server.js
+++ server.js
@@ -4,11 +4,12 @@
 // get all the tools we need
 var express  = require('express');
 var app      = express();
-var port     = process.env.PORT || 8080;
+var port     = process.env.PORT || serverConfig.port;
 var mongoose = require('mongoose');
 var passport = require('passport');
 var flash    = require('connect-flash');
 var path = require("path");
+var serverConfig = require("./serverFiles/config.js");
 
 var inter = require("./app/interceptor");
 
